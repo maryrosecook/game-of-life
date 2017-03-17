e74099cef47bca5f8c80ad38256b06901b2aa8bb
Mary Rose Cook <mary@maryrosecook.com>
2017-03-17 23:46:10 +0000

    Clarify code

--- lib/string_output.rb
+++ lib/string_output.rb
@@ -1,12 +1,18 @@
 module StringOutput
   def self.to_string(board)
     board.cell_states_by_line.map do |cell_states|
-      cell_strings = cell_states.map(&method(:cell_state_to_string))
-      "#{cell_strings.join}\n"
+      line_to_string(cell_states)
     end.join
   end
 
   private
+
+  def self.line_to_string(cell_states)
+    cell_strings = cell_states
+                   .map(&method(:cell_state_to_string))
+                   .join
+    "#{cell_strings}\n"
+  end
 
   def self.cell_state_to_string(cell_state)
     cell_state ? "O" : " "
