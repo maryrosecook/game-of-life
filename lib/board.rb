7a75934297d2ca0c1c06fab8507160acc5685843
Mary Rose Cook <mary@maryrosecook.com>
2017-03-24 13:32:08 +0000

    Add board.neighbours

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,8 @@
 class Board
   SIZE = 3
+  NEIGHBOUR_OFFSETS = [[-1, -1], [0, -1], [1, -1],
+                       [-1, 0], [1, 0],
+                       [-1, 1], [0, 1], [1, 1]]
 
   def initialize(living_cell_indices={}, cell_class=Cell)
     @cells = 9.times.map do |i|
@@ -8,8 +11,28 @@ class Board
   end
 
   def cell_states_by_line
-    @cells.each_slice(SIZE).to_a.map do |line_cells|
+    lines.map do |line_cells|
       line_cells.map(&:living?)
     end
+  end
+
+  def neighbours(cell_index)
+    hash_lines = to_h(lines.map(&method(:to_h)))
+    row = cell_index / SIZE
+    column = cell_index % SIZE
+
+    NEIGHBOUR_OFFSETS.map do |column_offset, row_offset|
+      (hash_lines[row + row_offset] || [])[column + column_offset]
+    end.compact
+  end
+
+  private
+
+  def to_h(array)
+    Hash[(0...array.size).zip(array)]
+  end
+
+  def lines
+    @cells.each_slice(SIZE).to_a
   end
 end
--- spec/units/board_spec.rb
+++ spec/units/board_spec.rb
@@ -30,4 +30,96 @@ describe Board do
                 [false, false, false]])
     end
   end
+
+  describe "#neighbours" do
+    let(:zero) { double(:zero_cell) }
+    let(:one) { double(:one_cell) }
+    let(:two) { double(:two_cell) }
+    let(:three) { double(:three_cell) }
+    let(:four) { double(:four_cell) }
+    let(:five) { double(:five_cell) }
+    let(:six) { double(:six_cell) }
+    let(:seven) { double(:seven_cell) }
+    let(:eight) { double(:eight_cell) }
+    let(:cell_class) { double(:cell_class) }
+
+    before(:each) do
+      allow(cell_class)
+        .to receive(:new).and_return(zero, one, two, three, four,
+                                     five, six, seven, eight)
+      @board = described_class.new({}, cell_class)
+    end
+
+    it "returns neighbours for top left cell" do
+      # xn2
+      # nn5
+      # 678
+      expect(@board.neighbours(0))
+        .to eq([one, three, four])
+    end
+
+    it "returns neighbours for top middle cell" do
+      # nxn
+      # nnn
+      # 678
+      expect(@board.neighbours(1))
+        .to eq([zero, two, three, four, five])
+    end
+
+    it "returns neighbours for top right cell" do
+      # 0nx
+      # 3nn
+      # 678
+      expect(@board.neighbours(2))
+        .to eq([one, four, five])
+    end
+
+    it "returns neighbours for left middle cell" do
+      # nn2
+      # xn5
+      # nn8
+      expect(@board.neighbours(3))
+        .to eq([zero, one, four, six, seven])
+    end
+
+    it "returns neighbours for middle middle cell" do
+      # nnn
+      # nxn
+      # nnn
+      expect(@board.neighbours(4))
+        .to eq([zero, one, two, three, five, six, seven, eight])
+    end
+
+    it "returns neighbours for middle right cell" do
+      # 0nn
+      # 3nx
+      # 6nn
+      expect(@board.neighbours(5))
+        .to eq([one, two, four, seven, eight])
+    end
+
+    it "returns neighbours for bottom left cell" do
+      # 012
+      # nn5
+      # xn8
+      expect(@board.neighbours(6))
+        .to eq([three, four, seven])
+    end
+
+    it "returns neighbours for bottom left cell" do
+      # 012
+      # nnn
+      # nxn
+      expect(@board.neighbours(7))
+        .to eq([three, four, five, six, eight])
+    end
+
+    it "returns neighbours for bottom left cell" do
+      # 012
+      # 3nn
+      # 6nx
+      expect(@board.neighbours(8))
+        .to eq([four, five, seven])
+    end
+  end
 end
